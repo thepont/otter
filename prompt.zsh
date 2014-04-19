1e78a6497d687a4122a945f66416a6fa9bad0662
Allan Hortle <allanhortle@gmail.com>
2014-04-19 17:47:36 +1000

    rad

--- prompt.zsh
+++ prompt.zsh
@@ -1,18 +1,11 @@
 
-# blue="%{$fg[blue]%}"
-# green="%{$fg[green]%}"
-# red="%{$fg[red]%}"
-# reset="%{$reset_color%}"
+blue="%{$fg[blue]%}"
+green="%{$fg[green]%}"
+red="%{$fg[red]%}"
+reset="%{$reset_color%}"
 
-# c2=$bwhite
-# c3=$grey
 # PROMPT="${blue}%c => ${reset}"
 # RPROMPT="${green}%M [%D{%H:%M:%S}]${reset}"
-
-# ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg[white]%}("
-# ZSH_THEME_GIT_PROMPT_SUFFIX=")%{$reset_color%}"
-# ZSH_THEME_GIT_PROMPT_DIRTY="*"
-# ZSH_THEME_GIT_PROMPT_CLEAN=""
 
 
 # # Default values for the appearance of the prompt. Configure at will.
@@ -43,4 +36,32 @@
 # ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$bg[cyan]%}%{$fg[black]%}…"
 # ZSH_THEME_GIT_PROMPT_CLEAN=""
 
-PROMPT=$'%{${fg[cyan]}%}%B%~%b$(prompt_git_info)%{${fg[default]}%} '
+ZSH_THEME_GIT_PROMPT_PREFIX=" $fg[white]‹ %{$fg_bold[yellow]%}"
+ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg_bold[magenta]%}↑"
+ZSH_THEME_GIT_PROMPT_STAGED="${green}●"
+ZSH_THEME_GIT_PROMPT_UNSTAGED="%{$fg_bold[red]%}●"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg_bold[white]%}●"
+ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg_bold[red]%}✕"
+ZSH_THEME_GIT_PROMPT_SUFFIX=" $fg_bold[white]›%{$reset_color%}"
+
+ZSH_THEME_GIT_PROMPT_ADDED="+"
+ZSH_THEME_GIT_PROMPT_AHEAD="↑"
+ZSH_THEME_GIT_PROMPT_AHEAD_REMOTE="aheadr"
+ZSH_THEME_GIT_PROMPT_BEHIND="↓"
+ZSH_THEME_GIT_PROMPT_BEHIND_REMOTE="BEHINDr"
+ZSH_THEME_GIT_PROMPT_CLEAN="clear"
+ZSH_THEME_GIT_PROMPT_DELETED="-"
+ZSH_THEME_GIT_PROMPT_DIRTY="..."
+ZSH_THEME_GIT_PROMPT_DIVERGED="↕"
+ZSH_THEME_GIT_PROMPT_DIVERGED_REMOTE="divR"
+ZSH_THEME_GIT_PROMPT_MODIFIED="●"
+ZSH_THEME_GIT_PROMPT_PREFIX="("
+ZSH_THEME_GIT_PROMPT_RENAMED="renamed"
+ZSH_THEME_GIT_PROMPT_SHA_AFTER=":"
+ZSH_THEME_GIT_PROMPT_SHA_BEFORE=":"
+ZSH_THEME_GIT_PROMPT_STASHED=">"
+ZSH_THEME_GIT_PROMPT_SUFFIX=")"
+ZSH_THEME_GIT_PROMPT_UNMERGED="✖"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="untracked"
+
+PROMPT=$'%{${fg[cyan]}%}%B%~%b$(git_prompt_status)%{${fg[default]}%} '
