e3d5fb7dd9eb2fdaab1955bb8b7f18a27fe6625d
Paul Esson <paul.esson@blueflag.com.au>
2015-08-23 11:56:30 +1000

    Change ip to allow different interface name

--- plugins/otter/functions.zsh
+++ plugins/otter/functions.zsh
@@ -5,11 +5,16 @@
 #--------------------------------------------------------------------------
 
 function ip() {
-    ifconfig lo0 | grep 'inet ' | sed -e 's/:/ /' | awk '{print "lo0       : " $2}'
-    ifconfig en0 | grep 'inet ' | sed -e 's/:/ /' | awk '{print "en0 (IPv4): " $2 " " $3 " " $4 " " $5 " " $6}'
-    ifconfig en0 | grep 'inet6 ' | sed -e 's/ / /'| awk '{print "en0 (IPv6): " $2 " " $3 " " $4 " " $5 " " $6}'
-    ifconfig en1 | grep 'inet ' | sed -e 's/:/ /' | awk '{print "en1 (IPv4): " $2 " " $3 " " $4 " " $5 " " $6}'
-    ifconfig en1 | grep 'inet6 ' | sed -e 's/ / /'| awk '{print "en1 (IPv6): " $2 " " $3 " " $4 " " $5 " " $6}'
+    interfaces=("${(@f)$(ifconfig -a | sed 's/[ \t].*//;/^$/d')}")
+    for interface in $interfaces; do
+        details=(`ifconfig $interface | grep 'inet ' | sed -e 's/:/ /'`)
+        detailsV6=(`ifconfig $interface | grep 'ineti6 ' | sed -e 's/:/ /'`)
+        
+        (( ${+details[2]} )) && 
+            print $interface "(IPv4)" $detailsi[2] $details[3] $details[4] $details[5] $details[6]
+        (( ${+detailsV6[2]} )) &&
+            print $interface "(IPv6)" $detailsV6[2] $detailsV6[3] $detailsV6[4] $detailsV6[5] $detailsiV6[6]
+    done
 }	
 
 
